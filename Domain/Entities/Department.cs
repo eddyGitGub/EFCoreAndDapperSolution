5b521539b1761532f55f7c007839bec0f075b69f
eddygitgub <eddycase1@gmail.com>
2020-11-08 23:15:02 +0100

    initial

---
+++ Domain/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿using CWM.DotNetCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Department : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public IDbConnection Connection => Database.GetDbConnection();
+    }
+}
